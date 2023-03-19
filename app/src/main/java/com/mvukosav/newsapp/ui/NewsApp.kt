7eb7dd3a4cda44ae83b80903eda47757e40bc36e
marko.vukosav <marko.vukosav@rimac-automobili.com>
2023-03-19 13:47:35 +0100

    first commit

---
+++ app/src/main/java/com/mvukosav/newsapp/ui/NewsApp.kt
@@ -0,0 +1,26 @@
+package com.mvukosav.newsapp.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.mvukosav.newsapp.ui.screen.DetailScreen
+import com.mvukosav.newsapp.ui.screen.TopNews
+
+@Composable
+fun NewsApp(){
+    Navigation()
+}
+
+@Composable
+fun Navigation(){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "TopNews"){
+        composable("TopNews"){
+            TopNews(navController = navController)
+        }
+        composable("Detail"){
+            DetailScreen(navController = navController)
+        }
+    }
+}
